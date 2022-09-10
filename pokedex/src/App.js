e77876038e1c1b3aa1ba6dad097f6dac7cda1ca8
jscucka <scucka.honza@gmail.com>
2022-09-10 17:12:08 +0200

    Changing app

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import "./Styles/App.css"
 import Header from './components/Header';
 import PokemonProvider from './components/PokemonProvider';
+
 function App() {
   return (
     <div className="App">
--- pokedex/src/components/PokemonCard.js
+++ pokedex/src/components/PokemonCard.js
@@ -1,12 +1,71 @@
 import React from "react";
+import "../Styles/PokemonCard.css";
 
-const PokemonCard = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h2>{props.name}</h2>
+const PokemonCard = props => {
+ 
+    const colours = {
+      normal: '#A8A77A',
+      fire: '#EE8130',
+      water: '#6390F0',
+      electric: '#F7D02C',
+      grass: '#7AC74C',
+      ice: '#96D9D6',
+      fighting: '#C22E28',
+      poison: '#A33EA1',
+      ground: '#E2BF65',
+      flying: '#A98FF3',
+      psychic: '#F95587',
+      bug: '#A6B91A',
+      rock: '#B6A136',
+      ghost: '#735797',
+      dragon: '#6F35FC',
+      dark: '#705746',
+      steel: '#B7B7CE',
+      fairy: '#D685AD',
+    };
+
+  return ( 
+    <div className="pokemonCard">
+      <div className="head" style={{background: colours[props.pokemonTypes[0]]}}>
+        <h2 id="pName">{props.pokemonName.toUpperCase()}</h2>
+        <h3 id="pIndex">#{props.pokemonId}</h3>
+      </div>
+    
+     <div className="container">
+      <div className="left">
+        <img src={props.image} alt={props.pokemonName}/>
+      </div>
+      <div className="right">
+        <div className="abilities">
+          <div className="strength">
+            <h4>Strong against</h4>
+            <ul>
+              {props.strongAgainst.map(s => <li key={s.name}>{s.name}</li>)}
+            </ul>
+          </div>
+          <div className="weakness">
+            <h4>Weak Against</h4>
+            <ul>
+              {props.weakAgainst.map(s => <li key={s.name}>{s.name}</li>)}
+            </ul>
+          </div>
         </div>
-    );
+        
+        
+        <div>
+        <form id="form"onSubmit={props.handleSubmit}>
+        
+        
+        
+        </form>
+        </div>
+      </div>
+     
+     </div>
+     <p className="lore">{props.pokemonFlavor}</p>
+     
+    </div>
+  );
 }
 
 export default PokemonCard;
